770ed6e71c37ba83e8980ad420227f01c783cc14
Rajesh <rajeshpal15076@gmail.com>
2025-11-03 15:16:59 +0530

    Fix: correct case for HeroSection import in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import HeroSection from '../../components/HeroSection'
-import About from '../../components/About'
-import Qualities from '../../components/Qualities'
-import Menu from '../../components/Menu'
-import WhoAreWe from '../../components/WhoAreWe'
-import Team from '../../components/Team'
-import Reservation from '../../components/Reservation'
-import Footer from '../../components/Footer'
+import HeroSection from '../components/HeroSection'
+import About from '../components/About'
+import Qualities from '../components/Qualities'
+import Menu from '../components/Menu'
+import WhoAreWe from '../components/WhoAreWe'
+import Team from '../components/Team'
+import Reservation from '../components/Reservation'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
