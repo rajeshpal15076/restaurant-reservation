1f8e4a9112205b61c6a3bbe067963000e2c0857e
Rajesh <rajeshpal15076@gmail.com>
2025-11-10 12:28:03 +0530

    modified

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -13,13 +13,11 @@ const About = () => {
               <p>The only thing we're serious about is food.</p>
             </div>
             <p className="mid">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Provident possimus optio adipisci dolores impedit illum iusto
-              perferendis, laudantium quod accusamus consequuntur consectetur,
-              tempore nulla error iure reiciendis dolorem assumenda.
-              Necessitatibus fugit asperiores totam rem esse exercitationem
-              iusto ipsum qui dolore ex, accusantium repellat mollitia
-              repellendus.
+              Welcome to our Restaurant Reservation System — where great taste
+              meets convenience! 🍽️ We offer super-tasty, high-quality food with
+              fast and reliable delivery, making every meal memorable. Our smart
+              platform lets you book tables effortlessly and enjoy a seamless
+              dining experience every time.
             </p>
             <Link to={"/"}>
               Explore Menu{" "}
